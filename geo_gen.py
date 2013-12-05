237b4e71e4e0ec8f4d0213191e3ff2c0603cbc35
dvtranmit <dvtran@mit.edu>
2013-12-05 17:59:49 -0500

    started creating the geometry code for nuclear data.

---
+++ geo_gen.py
@@ -0,0 +1,57 @@
+"""Imports all modules from OpenMOC, as well as the individual functions log, 
+plotter, and materialize, all of which are part of submodules within OpenMoc"""
+
+from openmoc import * 
+import openmoc.log as log # this module stores data printed during simulation
+import openmoc.plotter as plotter
+import openmoc.materialize as materialize
+
+#sets the number of energy groups
+numgroups = str(raw_input('How many energy groups? '))
+
+#sets geometry variable to the file name used
+geometry = raw_input('What is/are the file names? (Enter each one separated by a space without \'c4.\' or the file extension.) ')
+
+directory = "materials/%s-group/" % (numgroups)
+
+###############################################################################
+#######################   Main Simulation Parameters   ########################
+###############################################################################
+
+"""This imports a variety of variables from the options file. This should be 
+located within the OpenMOC folder.This could potentially also accept user input,
+but there should also be a default value."""
+
+num_threads = options.num_omp_threads
+track_spacing = options.track_spacing
+num_azim = options.num_azim
+tolerance = options.tolerance
+max_iters = options.max_iters
+
+log.setLogLevel('NORMAL')
+
+###############################################################################
+###########################   Creating Materials   ############################
+###############################################################################
+
+log.py_printf('NORMAL', 'Importing materials data from HDF5...')
+
+#The following assigns the dictionary returned by the materialize function in 
+#the materialize python file to the variable materials
+materials = materialize.materialize(directory + geometry + '-materials.hdf5')
+
+#jasmeet rox
+for material in materials:
+    material_id = materials[str(material)].getId()
+
+###############################################################################
+###########################   Creating Surfaces   #############################
+###############################################################################
+
+log.py_printf('NORMAL', 'Creating Surfaces...')
+
+#creates list of circle and plane surfaces
+circles = [] 
+planes = []
+
+
