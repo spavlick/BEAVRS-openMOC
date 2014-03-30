3ffba33c42ec919a27e61a3845af5f1eeececb9d
dvtranmit <dvtran@mit.edu>
2014-03-30 11:05:49 -0400

    single-run python files configured to compute pin powers now. running on cluster.

--- studies/pwru240c00/pwru240c00.py
+++ studies/pwru240c00/pwru240c00.py
@@ -36,14 +36,18 @@ lattice = createLattice(pwru240c00)
 geometry = createGeometry(geoDirectory, assembly_name, dummy, materials, cells, pinCellArray, lattice)
 track_generator = createTrackGen(num_azim, geometry, track_spacing)
 solver = createSolver(geometry, track_generator, num_threads, tolerance, max_iters)
-kinf = solver.getKeff()
+#kinf = solver.getKeff()
 
-# writes kinf to file
-f = h5py.File('../best-case-kinf-values.hdf5')
-f.attrs['Energy Groups'] = numgroups
-kinf_group = f.create_group('pwru240c00')
-kinf_group.create_dataset('kinf', data=kinf)
-f.close()
+## writes kinf to file
+#f = h5py.File('../best-case-kinf-values.hdf5')
+#f.attrs['Energy Groups'] = numgroups
+#kinf_group = f.create_group('pwru240c00')
+#kinf_group.create_dataset('kinf', data=kinf)
+#f.close()
+
+# stores pin error array
+
+process.compute_pin_powers(solver, use_hdf5=True)      
 
 #plotter.plot_fluxes(geometry, solver, energy_groups=[1,2], gridsize = 1200)
 
