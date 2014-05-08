2d3df851fad89da179d1979a154b8be845eaa40a
dvtranmit <dvtran@mit.edu>
2014-05-08 10:35:02 -0400

    Started README, currently working on cleaning files and making running studies a lot easier.

---
+++ README.txt
@@ -0,0 +1,43 @@
+===============================================================================
+===============================================================================
+~ Repository for Verification of OpenMOC using BEAVRS Full-Core Reactor Model ~
+===============================================================================
+===============================================================================
+
+
+
+
+===============================================================================
+                          AUTHORS & CONTACT INFORMATION
+===============================================================================
+
+Undergraduate Contributors-----------------------------------------------------
+
+    Davis Tran        -- dvtran@mit.edu
+    Stephanie Pavlick -- spavlick@mit.edu
+    Jasmeet Arora     -- jasmeet@mit.edu
+
+Graduate Mentor----------------------------------------------------------------
+
+    Will Boyd         -- wboyd@mit.edu
+
+===============================================================================
+                                 BRIEF OVERVIEW
+===============================================================================
+
+The BEAVRS-Openmoc repository contains files that use OpenMOC to develop
+convergence studies of five different assembly types in the BEAVRS Full Core
+reactor model.
+
+===============================================================================
+                               DETAILED OVERVIEW
+===============================================================================
+
+The BEAVRS-Openmoc repository uses the MIT Computational Reactor Physics
+(MIT-CRPG) Open-source Method of Characteristics reactor simulation code to run
+numerous simulations on reactor models. The specific models used were single
+17x17 assemblies in the BEAVRS full-core reactor model. The BEAVRS full-core 
+reactor model was also developed by the MIT-CRPG using real reactor data. 
+Cross-sectional materials data for the assemblies were generated using CASMO.
+The five assemblies contain various types of fuel enrichments and do or do not
+contain burnable poisons. After running numerous simulations 
--- studies/convergence-plotter.py
+++ studies/convergence-plotter.py
@@ -2,40 +2,52 @@ import h5py
 import numpy
 from tester import *
 
+
 assembly1 = 'pwru160c00'
 assembly2 = 'pwru240w12'
 
+#Open results file for assembly 1
 f = h5py.File('results/' + assembly1 + '-errors.h5', 'r')
+
+#Generates keys to index into azimuthal angle data sets
 keys_az = f['az'].keys()
 
+#Initialize line arrays for azimuthal angles errors
 angles = numpy.zeros(len(keys_az) / 3)
 az_max_errors1 = numpy.zeros(len(keys_az) / 3)
 az_mean_errors1 = numpy.zeros(len(keys_az) / 3)
 az_kinf_errors1 = numpy.zeros(len(keys_az) / 3)
 
+#Append num_azim to angles array and append kinf errors to az_kinf_errors1
 for key in keys_az:
     if "kinf" in key:
         num_azim = int(key.strip("az_kinf_num_azim = "))
         angles[num_azim / 4 - 1] = num_azim
         az_kinf_errors1[num_azim / 4 - 1] = f['az'][key][...] * 10**5
 
+#Append max errors to az_max_errors1
 for key in keys_az:
     if "max" in key:
         num_azim = int(key.strip("az_max_num_azim = "))
         az_max_errors1[num_azim / 4 - 1] = f['az'][key][...] * 10**2
 
+#Append mean errors to az_mean_errors1
 for key in keys_az:
     if "mean" in key:
         num_azim = int(key.strip("az_mean_num_azim = "))
         az_mean_errors1[num_azim / 4 - 1] = f['az'][key][...] * 10**2
 
-
+#Generate keys to index into track spacing data sets
 keys_ts = f['ts'].keys()
 
+#Initialize line arrays for track spacing erros
 tracks = numpy.zeros(len(keys_ts) / 3)
 ts_max_errors1 = numpy.zeros(len(keys_ts) / 3)
 ts_mean_errors1 = numpy.zeros(len(keys_ts) / 3)
 ts_kinf_errors1 = numpy.zeros(len(keys_ts) / 3)
+
+#Append track_spacing to tracks array and append kinf error to
+#ts_kinf_errors1
 counter = 0
 for key in keys_ts:
     if "kinf" in key:
@@ -44,6 +56,7 @@ for key in keys_ts:
         ts_kinf_errors1[counter] = f['ts'][key][...] * 10**5
         counter += 1
 
+#Append max errors to ts_kinf_errors1
 counter = 0
 for key in keys_ts:
     if "max" in key:
@@ -51,6 +64,7 @@ for key in keys_ts:
         ts_max_errors1[counter] = f['ts'][key][...] * 10**2
         counter += 1
 
+#Append mean errors to ts_kinf_errors1
 counter = 0
 for key in keys_ts:
     if "mean" in key:
@@ -60,7 +74,10 @@ for key in keys_ts:
 
 f.close()
 
+#Open results file for assembly 2
 f = h5py.File('results/' + assembly2 + '-errors.h5', 'r')
+
+
 keys_az = f['az'].keys()
 
 az_max_errors2 = numpy.zeros(len(keys_az) / 3)
@@ -112,21 +129,69 @@ for key in keys_ts:
 f.close()
 
 
+#Uses tester.py plotter method to plot convergence graphs in matplotlib
+plotter([angles, angles],
+        [az_kinf_errors1,az_kinf_errors2],
+        "Azimuthal Angle Convergence Study",
+        "Azimuthal Angles",
+        "Error in K-effective [pcm]",
+        max(angles),
+        max(az_kinf_errors1) + 10,
+        "kinf-angles.png",
+        2,
+        legend = ['Without BP', 'With BP'])
 
-plotter([angles, angles], [az_kinf_errors1, az_kinf_errors2], "Azimuthal Angle Convergence Study", \
-"Azimuthal Angles", "Error in K-effective [pcm]", max(angles), max(az_kinf_errors1) + 10, "kinf-angles.png", 2, legend = ['Without BP', 'With BP'])
+plotter([angles, angles],
+        [az_max_errors1, az_mean_errors1],
+        "Relative Percent Pin Power Error Without BP",
+        "Azimuthal Angles",
+        "Pin Power Errors",
+        max(angles),
+        max(az_max_errors1) + .2,
+        assembly1 + "-powers-angles.png",
+        2,
+        legend = ['Max', 'Mean'])
 
-plotter([angles, angles], [az_max_errors1, az_mean_errors1], "Relative Percent Pin Power Error Without BP", \
-"Azimuthal Angles", "Pin Power Errors", max(angles), max(az_max_errors1) + .2, assembly1 + "-powers-angles.png", 2, legend = ['Max', 'Mean'])
+plotter([angles, angles],
+        [az_max_errors2, az_mean_errors2],
+        "Relative Percent Pin Power Error With BP",
+        "Azimuthal Angles",
+        "Pin Power Errors",
+        max(angles),
+        max(az_max_errors2) + .2,
+        assembly2 + "-powers-angles.png",
+        2,
+        legend = ['Max', 'Mean'])
 
-plotter([angles, angles], [az_max_errors2, az_mean_errors2], "Relative Percent Pin Power Error With BP", \
-"Azimuthal Angles", "Pin Power Errors", max(angles), max(az_max_errors2) + .2, assembly2 + "-powers-angles.png", 2, legend = ['Max', 'Mean'])
+plotter([tracks,tracks],
+        [ts_kinf_errors1, ts_kinf_errors2],
+        "Track Spacing Convergence Study",
+        "Track Spacing",
+        "Error in K-effective [pcm]",
+        max(tracks),
+        max(ts_kinf_errors1) + 10,
+        "kinf-tracks.png",
+        2,
+        legend = ['Without BP', 'With BP'])
 
-plotter([tracks,tracks], [ts_kinf_errors1, ts_kinf_errors2], "Track Spacing Convergence Study", \
-"Track Spacing", "Error in K-effective [pcm]", max(tracks), max(ts_kinf_errors1) + 10, "kinf-tracks.png", 2, legend = ['Without BP', 'With BP'])
+plotter([tracks, tracks],
+        [ts_max_errors1, ts_mean_errors1],
+        "Relative Percent Pin Power Error Without BP",
+        "Track Spacing",
+        "Pin Power Errors",
+        max(tracks),
+        max(ts_max_errors1) + .2,
+        assembly1 + "-powers-tracks.png",
+        2,
+        legend = ['Max', 'Mean'])
 
-plotter([tracks, tracks], [ts_max_errors1, ts_mean_errors1], "Relative Percent Pin Power Error Without BP", \
-"Track Spacing", "Pin Power Errors", max(tracks), max(ts_max_errors1) + .2, assembly1 + "-powers-tracks.png", 2, legend = ['Max', 'Mean'])
-
-plotter([tracks, tracks], [ts_max_errors2, ts_mean_errors2], "Relative Percent Pin Power Error With BP", \
-"Track Spacing", "Pin Power Errors", max(tracks), max(ts_max_errors2) + .2, assembly2 + "-powers-tracks.png", 2, legend = ['Max', 'Mean'])
+plotter([tracks, tracks],
+        [ts_max_errors2, ts_mean_errors2],
+        "Relative Percent Pin Power Error With BP",
+        "Track Spacing",
+        "Pin Power Errors",
+        max(tracks),
+        max(ts_max_errors2) + .2,
+        assembly2 + "-powers-tracks.png",
+        2,
+        legend = ['Max', 'Mean'])
